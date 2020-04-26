1f81d6ed14d1aa1e97ee4c29c26b5059d6e7bad6
WilliamCrewe <52168122+WilliamCrewe@users.noreply.github.com>
2020-04-26 18:53:49 +0100

    Attempted Fix

---
+++ src/main/graphical_interface/util/GUILocation.java
@@ -0,0 +1,57 @@
+package main.graphical_interface.util;
+
+public class GUILocation {
+	
+	private Command command;
+	private String name;
+	private int page;
+	private String locationID;
+	
+	public GUILocation(Command command, String name, int page) {
+		this.command = command;
+		this.name = name;
+		this.page = page;
+	}
+	
+	public GUILocation() {
+		this.command = null;
+		this.name = null;
+		this.locationID = null;
+		this.page = -1;
+	}
+
+	public Command getCommand() {
+		return command;
+	}
+
+	public void setCommand(Command command) {
+		this.command = command;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPage() {
+		return page;
+	}
+
+	public void setPage(int page) {
+		this.page = page;
+	}
+
+	public String getLocationID() {
+		return locationID;
+	}
+
+	public void setLocationID(String locationID) {
+		this.locationID = locationID;
+	}
+	
+	
+
+}
